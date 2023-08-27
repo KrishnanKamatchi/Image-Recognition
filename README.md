b6b404f8ad613b942c0ab532a23a2c16b66bc9ce
krishna <krishna@shravtek.com>
2023-08-27 22:19:29 +0530

    Image Recognition

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ocr
+# Image Reader
+
+This project build on Tensorflow models(coco-ssd), express framework to host server, so that we can connect API.
+
+1. Download project files into your local.
+2. Run 'npm i' to install require packages.
+3. Run 'npm start' or 'npm run dev'(for nodemon) to launch server.
+4. Create .env file with property PORT, else server will launch on port- 4200.
+5. Post an form data with image file, at path http://localhost:4200/predict.
+6. Response json will receive with output data from predicted percentage value from models.
+
+Credit: https://betterprogramming.pub/node-js-implementation-of-image-recognition-using-tensorflow-and-express-js-b006f5609415
---
+++ index.js
@@ -0,0 +1,56 @@
+// * TensorFlow Stuff
+import * as tf from "@tensorflow/tfjs-node";
+import coco_ssd from "@tensorflow-models/coco-ssd";
+
+// * Server Stuff
+import express from "express";
+import busboy from "busboy";
+import { config } from "dotenv";
+import cors from "cors";
+config();
+
+// * Filesystem Stuff
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// * Init Model
+let model = undefined;
+(async () => {
+  model = await coco_ssd.load({
+    base: "mobilenet_v1",
+  });
+})();
+
+// * Init Express
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.post("/predict", (req, res) => {
+  if (!model) {
+    res.status(500).send("Model is not loaded yet!");
+    return;
+  }
+
+  // * Create a Busboy instance
+  const bb = busboy({ headers: req.headers });
+  bb.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    const buffer = [];
+    file.on("data", (data) => {
+      buffer.push(data);
+    });
+    file.on("end", async () => {
+      // * Run Object Detection
+      const image = tf.node.decodeImage(Buffer.concat(buffer));
+      const predictions = await model.detect(image, 3, 0.25);
+      res.json(predictions);
+    });
+  });
+  req.pipe(bb);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
